95178da7636f05595864d46e334a71d390d08c77
Jalal Kiswani <kiswanij@gmail.com>
2020-07-07 10:17:43 +0300

    updated version

--- Example1/Test.java
+++ Example1/Test.java
@@ -1,6 +1,6 @@
 public class Test{
 	
 		public static void main(String args[]){
-			System.out.println("Hello form uncle Jalal");
+			System.out.println("Hello form uncle Jalal, version 2");
 		}
 }
