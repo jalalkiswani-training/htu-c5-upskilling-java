63b6ee3c73aab7d542aa1a6de1b1ae23db7a757f
Hana Rasheed <hana.alrasheed@htu.edu.jo>
2020-07-14 14:12:09 +0300

    lecture 3 Tasks

---
+++ src/edu/htu/upskilling/exercises/ArrayExercise.java
@@ -0,0 +1,41 @@
+package edu.htu.upskilling.exercises;
+
+public class ArrayExercise {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * // data type variable name= value; int x=5;
+		 * 
+		 * // 5 subjects int grade1=5; int grade2=5; int grade3=6;
+		 * 
+		 * int [] grades=new int [5]; // size =5 // indexes -> 0 -> 4 for(int i=0;
+		 * i<grades.length;i++) { grades[i]=5;
+		 * 
+		 * }
+		 */
+		
+		/*1- Write a program to take student name 
+		 *2- enter enter number of subjects
+		 *3- enter grade for each subject
+		 *4- calculate summation
+		 *5- calculate GPA
+		 *6- print student name, summation and GPA  
+		
+		
+		*/
+
+		
+		
+		/*
+		 * write a program to find the maximum value of 5 numbers entered by the user.
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	}
+
+}
