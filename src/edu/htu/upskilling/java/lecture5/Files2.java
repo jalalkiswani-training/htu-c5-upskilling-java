3355b82bcd3705c838623c0c1d81494c4bd5d814
Jalal Kiswani <kiswanij@gmail.com>
2020-07-21 11:46:05 +0300

    Files and Methods

---
+++ src/edu/htu/upskilling/java/lecture5/Files2.java
@@ -0,0 +1,26 @@
+package edu.htu.upskilling.java.lecture5;
+
+public class Files2 {
+	public static void main(String[] args) {
+		String newLine=System.getProperty("line.separator");
+		String marksString=Files1.readFile("marks.txt");
+		String students[]=marksString.split(newLine);
+		
+		for (int i = 0; i < students.length; i++) {
+			String student = students[i];
+			System.out.println("Student Info: "+student);			
+		}
+		
+		for (String student: students) {
+			String[] studentInfo = student.split(",");
+			String name=studentInfo[0];
+			int sum=0;
+			for(int i=1;i<studentInfo.length;i++) {
+				int mark=Integer.parseInt(studentInfo[i]);
+				sum+=mark;				
+			}
+			int avg=sum/(studentInfo.length-1);
+			System.out.println("Student "+name+" sum ="+sum+" avg= "+avg);
+		}
+	}
+}
---
+++ src/edu/htu/upskilling/java/lecture5/Methods2.java
@@ -0,0 +1,10 @@
+package edu.htu.upskilling.java.lecture5;
+
+public class Methods2 {
+	public static void main(String[] args) {
+		int n1 = Methods1.readNumber();
+		int n2=Methods1.readNumber();
+		
+		Methods1.printMessage((n1*n2)+"");
+	}
+}
